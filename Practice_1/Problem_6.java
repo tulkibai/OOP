eb722c886428fae96858da37e61514ecdcfdfcb6
Kudaibergen <k_tukubayev@kbtu.kz>
2026-01-28 09:11:11 +0500

    Third part of Practice_1

--- Practice_1/Problem_6.java
+++ Practice_1/Problem_6.java
@@ -8,6 +8,14 @@ void main() {
     else IO.print("Is not palindrome");
 }
 
+String rever_se(String str) {
+    String ret_urn = "";
+    for (int i = str.length() - 1; i >= 0; --i) {
+        ret_urn += str.charAt(i);
+    }
+    return ret_urn;
+}
+
 boolean isPalindrome(String a) {
     String s1, s2;
     if (a.length() % 2 == 0) {
@@ -18,7 +26,7 @@ boolean isPalindrome(String a) {
         s1 = a.substring(0, (a.length() + 1) / 2);
         s2 = a.substring((a.length() + 1) / 2 - 1);
     }
-    s2 = new StringBuilder(s2).reverse().toString();
+    s2 = rever_se(s2);
 
     return s1.equals(s2);
 }
